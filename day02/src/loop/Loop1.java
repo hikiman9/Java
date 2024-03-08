94b9e668f9928591e4f37b6845ea3d5d1657f8e9
hikiman <skyainq@naver.com>
2024-03-08 10:28:04 +0900

    240308(Git 환경 세팅)

---
+++ day02/src/loop/Loop1.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("wffwe");
+        }
+        int sum2 = 0;
+        int j = 1;
+        while(j <= 10){
+            sum2 += j;
+            j += 1;
+            System.out.println(sum2);
+        }
+    }
+}
