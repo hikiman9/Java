9bf409760a20430ad11dca579c3c7ff563da2eab
hikiman <skyainq@naver.com>
2024-03-08 11:19:28 +0900

    240308(배열)

---
+++ day02/src/array/Array1.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int a [];
+        a = new int[10];
+
+        int [] b;
+        b = new int[] {1, 2, 3, 4, 5};
+
+        int [] c  = {1, 2, 3, 4, 5};
+
+    }
+}
